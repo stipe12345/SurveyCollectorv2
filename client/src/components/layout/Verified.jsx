3cccb46559abd8df6f611867fc30abccc27462f4
stipe12345 <stipe.capko@hotmail.com>
2021-09-30 18:09:44 +0200

    email verification

--- client/src/components/layout/Verified.jsx
+++ client/src/components/layout/Verified.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 const Verified=(props)=>{
-
-    return <>test
+const {email}=useParams();
+const {token}=useParams();
+    return <>{email}
     </>
 }
 export default Verified;
